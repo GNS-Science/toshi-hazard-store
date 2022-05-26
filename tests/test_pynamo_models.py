93836044685175ddaa4e950600e684936f91c3b6
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-05-27 11:36:09 +1200

    added meta table and simplce create test

--- tests/test_pynamo_models.py
+++ tests/test_pynamo_models.py
@@ -1,8 +1,11 @@
 import unittest
+import json
 
 from moto import mock_dynamodb
 from toshi_hazard_store import model
+import datetime as dt
 
+from dateutil.tz import tzutc
 
 @mock_dynamodb
 class PynamoTest(unittest.TestCase):
@@ -71,3 +74,34 @@ class PynamoTest(unittest.TestCase):
         self.assertEqual(obj.lvl_val_pairs[0].value, 0.000001)
         self.assertEqual(obj.lvl_val_pairs[9].level, 0.01)
         self.assertEqual(obj.lvl_val_pairs[9].value, 0.00001)
+
+
+
+    def test_save_one_meta_object(self):
+
+        obj = model.ToshiOpenquakeHazardMeta(
+            hazard_solution_id = "UnicodeAttribute(hash_key=True)",
+            hazsol_vs30_rk = "UnicodeAttribute(range_key=True)",
+            created = dt.datetime.now(tzutc()),
+             # known at configuration
+            vs30 = 350,                         # vs30 value
+            imt_codes = ['PGA', 'SA(0.5)'],     # list of IMTs
+            loc_codes = ['WLG', 'AKL'],         # list of Location codes
+            source_models = ['A', 'B'],         # list of source model ids
+            # extracted from the OQ HDF5
+            source_df = json.dumps(dict(sources=[1,2])),  # sources meta as DataFrame JSON
+            gsim_df = json.dumps(dict(gsims=[1,2])),   # gmpe meta as DataFrame JSON
+            rlzs_df = json.dumps(dict(rlzs=[1,2]))    # realization meta as DataFrame JSON
+        )
+
+        print(f'obj: {obj} {obj.version}')
+        self.assertEqual(obj.version, None)
+
+        obj.save()
+        self.assertEqual(obj.version, 1)
+
+        # self.assertEqual(obj.lvl_val_pairs[0].level, 0.001)
+        # self.assertEqual(obj.lvl_val_pairs[0].value, 0.000001)
+        # self.assertEqual(obj.lvl_val_pairs[9].level, 0.01)
+        # self.assertEqual(obj.lvl_val_pairs[9].value, 0.00001)
+
--- toshi_hazard_store/model.py
+++ toshi_hazard_store/model.py
@@ -2,12 +2,43 @@
 
 import logging
 
-from pynamodb.attributes import ListAttribute, MapAttribute, NumberAttribute, UnicodeAttribute, VersionAttribute
+from pynamodb.attributes import ( ListAttribute, MapAttribute, NumberAttribute, UnicodeAttribute, VersionAttribute,
+    UTCDateTimeAttribute, JSONAttribute, UnicodeSetAttribute )
 from pynamodb.models import Model
 
 from toshi_hazard_store.config import DEPLOYMENT_STAGE, IS_OFFLINE, REGION
 
 log = logging.getLogger(__name__)
+
+
+class ToshiOpenquakeHazardMeta(Model):
+    """Stores metadata from the job configuration and the oq HDF5."""
+
+    class Meta:
+        """DynamoDB Metadata."""
+
+        billing_mode = 'PAY_PER_REQUEST'
+        table_name = f"ToshiOpenquakeHazardMeta-{DEPLOYMENT_STAGE}"
+        region = REGION
+        if IS_OFFLINE:
+            host = "http://localhost:8000"  # pragma: no cover
+
+    hazard_solution_id = UnicodeAttribute(hash_key=True)
+    hazsol_vs30_rk = UnicodeAttribute(range_key=True)
+
+    created = UTCDateTimeAttribute()
+    version = VersionAttribute()
+
+    # known at configuration
+    vs30 = NumberAttribute()              # vs30 value
+    imt_codes = UnicodeSetAttribute()     # list of IMTs
+    loc_codes = UnicodeSetAttribute()     # list of Location codes
+    source_models = UnicodeSetAttribute() # list of source model ids
+
+    # extracted from the OQ HDF5
+    source_df = JSONAttribute() # sources meta as DataFrame JSON
+    gsim_df = JSONAttribute()   # gmpe meta as DataFrame JSON
+    rlzs_df = JSONAttribute()   # realization meta as DataFrame JSON
 
 
 class LevelValuePairAttribute(MapAttribute):
@@ -65,11 +96,11 @@ class ToshiOpenquakeHazardCurveStats(Model):
     version = VersionAttribute()
 
 
-tables = [ToshiOpenquakeHazardCurveRlzs, ToshiOpenquakeHazardCurveStats]
+tables = [ToshiOpenquakeHazardCurveRlzs, ToshiOpenquakeHazardCurveStats, ToshiOpenquakeHazardMeta]
 
 
 def migrate():
-    """Create the tables if it doesn't exist already."""
+    """Create the tables, unless they exist already."""
     for table in tables:
         if not table.exists():  # pragma: no cover
             table.create_table(wait=True)
@@ -78,7 +109,7 @@ def migrate():
 
 
 def drop_tables():
-    """Drop the tables if they exist."""
+    """Drop the tables, if they exist."""
     for table in tables:
         if table.exists():  # pragma: no cover
             table.delete_table()
