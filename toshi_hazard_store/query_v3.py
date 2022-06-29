b396f21cfd18c9bdf4d5dab82e925a32e9bfadc8
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-06-29 13:45:43 +1200

    query improvements

--- toshi_hazard_store/query_v3.py
+++ toshi_hazard_store/query_v3.py
@@ -48,17 +48,21 @@ def get_rlz_curves_v3(
     f'{nloc_001}:{vs30s}:{rlzs}:{self.hazard_solution_id}'
     """
 
-    sort_key_first_val = ""
-    condition_expr = None
-
-    if locs:
+    def build_condition_expr(locs, vs30s, rlzs, tids):
+        """Build filter condition."""
         ## TODO REFACTOR ME ... using the res of first loc is not ideal
-        grid_res = decimal.Decimal(str(list(locs)[0].split('~')[0]).rstrip("0"))
+        grid_res = decimal.Decimal(str(list(locs)[0].split('~')[0]))
         places = grid_res.as_tuple().exponent
-        # print(f'places {places} loc {locs[0]}')
+
+        # print()
+        # print(f'places {places} loc[0] {locs[0]}')
 
         res = float(decimal.Decimal(10) ** places)
         locs = [downsample_code(loc, res) for loc in locs]
+
+        # print()
+        # print(f'res {res} locs {locs}')
+        condition_expr = None
 
         if places == -1:
             condition_expr = condition_expr & mRLZ.nloc_1.is_in(*locs)
@@ -67,25 +71,31 @@ def get_rlz_curves_v3(
         if places == -3:
             condition_expr = condition_expr & mRLZ.nloc_001.is_in(*locs)
 
-    if vs30s:
-        condition_expr = condition_expr & mRLZ.vs30.is_in(*vs30s)
-    if rlzs:
-        condition_expr = condition_expr & mRLZ.rlz.is_in(*rlzs)
-    if tids:
-        condition_expr = condition_expr & mRLZ.hazard_solution_id.is_in(*tids)
+        if vs30s:
+            condition_expr = condition_expr & mRLZ.vs30.is_in(*vs30s)
+        if rlzs:
+            condition_expr = condition_expr & mRLZ.rlz.is_in(*rlzs)
+        if tids:
+            condition_expr = condition_expr & mRLZ.hazard_solution_id.is_in(*tids)
 
-    if locs:
-        first_loc = downsample_code(sorted(locs)[0], 0.001)  # these need to be formatted to match the sort key 0.001 ?
+        return condition_expr
+
+    def build_sort_key(locs, vs30s, rlzs, tids):
+        """Build sort_key."""
+        sort_key_first_val = ""
+        first_loc = sorted(locs)[0]  # these need to be formatted to match the sort key 0.001 ?
         sort_key_first_val += f"{first_loc}"
-    if locs and vs30s:
-        first_vs30 = sorted(vs30s)[0]
-        sort_key_first_val += f":{first_vs30}"
-    if locs and vs30s and rlzs:
-        first_rlz = str(sorted(rlzs)[0]).zfill(6)
-        sort_key_first_val += f":{first_rlz}"
-    if locs and vs30s and rlzs and tids:
-        first_tid = sorted(tids)[0]
-        sort_key_first_val += f":{first_tid}"
+
+        if vs30s:
+            first_vs30 = sorted(vs30s)[0]
+            sort_key_first_val += f":{first_vs30}"
+        if vs30s and rlzs:
+            first_rlz = str(sorted(rlzs)[0]).zfill(6)
+            sort_key_first_val += f":{first_rlz}"
+        if vs30s and rlzs and tids:
+            first_tid = sorted(tids)[0]
+            sort_key_first_val += f":{first_tid}"
+        return sort_key_first_val
 
     def get_hashes(locs):
         hashes = set()
@@ -95,15 +105,26 @@ def get_rlz_curves_v3(
             hashes.add(downsample_code(loc, 0.1))
         return list(hashes)
 
-    print('hashes', get_hashes(locs))
-
+    # print('hashes', get_hashes(locs))
     # TODO: use https://pypi.org/project/InPynamoDB/
-
     for hash_location_code in get_hashes(locs):
 
-        print(f'hash_key {hash_location_code}')
-        print(f'sort_key_first_val: {sort_key_first_val}')
-        print(f'condition_expr: {condition_expr}')
+        # print(f'hash_key {hash_location_code}')
+
+        hash_locs = list(filter(lambda loc: downsample_code(loc, 0.1) == hash_location_code, locs))
+
+        sort_key_first_val = build_sort_key(hash_locs, vs30s, rlzs, tids)
+        condition_expr = build_condition_expr(hash_locs, vs30s, rlzs, tids)
+
+        # print(f'sort_key_first_val: {sort_key_first_val}')
+        # print(f'condition_expr: {condition_expr}')
+
+        # expected_sort_key = '-41.300~174.780:750:000000:A_CRU'
+        # expected_hash_key = '-41.3~174.8'
+
+        # print()
+        # print(expected_hash_key, expected_sort_key)
+        # # assert 0
 
         if sort_key_first_val:
             qry = mRLZ.query(hash_location_code, mRLZ.sort_key >= sort_key_first_val, filter_condition=condition_expr)
@@ -114,7 +135,7 @@ def get_rlz_curves_v3(
                 filter_condition=condition_expr,
             )
 
-        print(f"get_hazard_rlz_curves_v3: qry {qry}")
+        # print(f"get_hazard_rlz_curves_v3: qry {qry}")
         for hit in qry:
             if imts:
                 hit.values = list(filter(lambda x: x.imt in imts, hit.values))
