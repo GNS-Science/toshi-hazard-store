8fb578e487bfe0fc5886ecb9292dbefdaf5e6daa
Chris B Chamberlain <chrisbc@artisan.co.nz>
2025-03-06 16:21:57 +1300

    Feature/add partitioning option to defrag (#71)
    * minor tweaks to demo
    * add parts option to defrag script;
    * add some instrumentation to defrag;
    * detox;

--- scripts/ths_r4_defrag.py
+++ scripts/ths_r4_defrag.py
@@ -1,30 +1,30 @@
-# flake8: noqa
 """
 Console script for compacting THS rev4 parquet datasets
 """
 
 import csv
-import datetime as dt
 import logging
-import os
 import pathlib
 import uuid
 from functools import partial
 
-# import time
 import click
-import pandas as pd
 import pyarrow as pa
-
-# import pyarrow.parquet as pq
 import pyarrow.compute as pc
 import pyarrow.dataset as ds
-import pytz
 from pyarrow import fs
+
+DATASET_FORMAT = 'parquet'  # TODO: make this an argument
+MEMORY_WARNING_BYTES = 8e9  # At 8 GB let the user know they might run into trouble!!!
 
 log = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.INFO)
+
+
+def human_size(bytes, units=[' bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']):
+    """Returns a human readable string representation of bytes"""
+    return str(bytes) + units[0] if bytes < 1024 else human_size(bytes >> 10, units[1:])
 
 
 def write_metadata(base_path, visited_file):
@@ -59,15 +59,20 @@ def write_metadata(base_path, visited_file):
 @click.command()
 @click.argument('source')
 @click.argument('target')
+@click.option("-p", "--parts", help="comma-separated list of partition keys", default="")
 @click.option('-v', '--verbose', is_flag=True, default=False)
 @click.option('-d', '--dry-run', is_flag=True, default=False)
 def main(
     source,
     target,
+    parts,
     verbose,
     dry_run,
 ):
-    """Compact the realisations dataset within each loc0 partition"""
+    """Compact the dataset within each partition.
+
+    Can be used on both realisation and aggregate datasets.
+    """
     source_folder = pathlib.Path(source)
     target_folder = pathlib.Path(target)
     target_parent = target_folder.parent
@@ -78,10 +83,11 @@ def main(
     assert target_parent.exists(), f'folder {target_parent} is not found'
     assert target_parent.is_dir(), f'folder {target_parent} is not a directory'
 
-    DATASET_FORMAT = 'parquet'  # TODO: make this an argument
-    BAIL_AFTER = 0  # 0 => don't bail
+    partition_keys = [part.strip() for part in parts.split(",")] if parts else []
 
-    # no optimising parallel stuff yet
+    if verbose:
+        click.echo(f"partitions: {partition_keys}")
+
     filesystem = fs.LocalFileSystem()
     dataset = ds.dataset(source_folder, filesystem=filesystem, format=DATASET_FORMAT, partitioning='hive')
 
@@ -89,26 +95,30 @@ def main(
 
     count = 0
     for partition_folder in source_folder.iterdir():
+        usage = sum(file.stat().st_size for file in partition_folder.rglob('*'))
+        if usage > MEMORY_WARNING_BYTES:
+            click.echo(f'partition {partition_folder} has size: {human_size(usage)}')
+            click.confirm('Do you want to continue?', abort=True)
+        elif verbose:
+            click.echo(f'partition {partition_folder} has disk size: {human_size(usage)}')
 
-        flt0 = pc.field('nloc_0') == pc.scalar(partition_folder.name.split('=')[1])
-        click.echo(f'partition {str(flt0)}')
+        _name, _value = partition_folder.name.split('=')
+        flt0 = pc.field(_name) == pc.scalar(_value)
 
         arrow_scanner = ds.Scanner.from_dataset(dataset, filter=flt0)
-        # table = arrow_scanner.to_table()
-
         ds.write_dataset(
             arrow_scanner,
             base_dir=str(target_folder),
             basename_template="%s-part-{i}.%s" % (uuid.uuid4(), DATASET_FORMAT),
-            partitioning=['nloc_0', 'imt'],  # TODO: make this an argument
+            partitioning=partition_keys,
             partitioning_flavor="hive",
             existing_data_behavior="delete_matching",
             format=DATASET_FORMAT,
             file_visitor=writemeta_fn,
         )
         count += 1
-
-        click.echo(f'compacted {target_folder}')
+        if verbose:
+            click.echo(f'pyarrow RSS memory: {human_size(pa.total_allocated_bytes())}')
 
     click.echo(f'compacted {count} partitions for {target_folder.parent}')
 
