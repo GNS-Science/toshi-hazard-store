b99401057774f0345375982306e77815b969f7c1
Chris B Chamberlain <chrisbc@artisan.co.nz>
2025-07-22 16:34:08 +1200

    tidy up scriptl simplify with get_locations();

--- toshi_hazard_store/scripts/migration/haz_agg/check_aggs_exist.py
+++ toshi_hazard_store/scripts/migration/haz_agg/check_aggs_exist.py
@@ -7,38 +7,24 @@ Look for evidence of any missing aggregations across
  - all types of locaion (grid, NZ, SWRG)
 """
 
-import inspect
 import logging
-import os
-import pathlib
 import random
-import sys
-import time
 
-import pyarrow as pa
-import pyarrow.compute as pc
-import pyarrow.dataset as ds
-from nzshm_common import CodedLocation, LatLon, location
-from nzshm_common.grids import load_grid
+from nzshm_common.location import get_locations
 from pyarrow import fs
 
 from toshi_hazard_store.query import datasets
 
 log = logging.getLogger(__name__)
 
-nz1_grid = load_grid('NZ_0_1_NB_1_1')
-city_locs = [
-    CodedLocation(lat=key.lat, lon=key.lon, resolution=0.001).code for key in location.get_location_list(["NZ"])
-]
-srwg_locs = [
-    CodedLocation(lat=key.lat, lon=key.lon, resolution=0.001).code for key in location.get_location_list(["SRWG214"])
-]
+nz1_grid = [loc.code for loc in get_locations(['NZ_0_1_NB_1_1'])]
+city_locs = [loc.code for loc in get_locations(['NZ'])]
+srwg_locs = [loc.code for loc in get_locations(['SRWG214'])]
 
-# vs30s = [400, 1000, 1500]
-vs30s = [ 375, 450, 500, 525, 600, 750, 900]
-# vs30s = [1500, 1000]
+vs30s = [400, 1000, 1500]
+vs30s += [375, 450, 500, 525, 600, 750, 900]
+vs30s += [150, 175, 200, 225, 250, 275, 300, 350]
 
-# GOOD: 150, 175, 200, 225, 250, 275, 300, 350,
 
 def check(get_locations_fn, test_label="NZ cities"):
     """Check that the correct number of curves are returned"""
@@ -77,15 +63,12 @@ def check(get_locations_fn, test_label="NZ cities"):
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.WARNING)
-    # # main_cites_test
-    # check(lambda: random.sample(city_locs, 10))
-
-    # # SRWG test
-    check(lambda: random.sample(srwg_locs, 10), test_label="10 of SRWG214")
 
     # # GRID test
-    def random_grid(n=10):
-        # at res = 1 everython misses ??
-        return [CodedLocation(lat=loc[0], lon=loc[1], resolution=0.1).code for loc in random.sample(nz1_grid, 10)]
+    check(lambda: random.sample(nz1_grid, 10), test_label="1O of NZ grid")
 
-    check(random_grid, test_label="1O of NZ grid")
+    # NZ cities test
+    check(lambda: random.sample(city_locs, 5))
+
+    # SRWG test
+    check(lambda: random.sample(srwg_locs, 5), test_label="10 of SRWG214")
