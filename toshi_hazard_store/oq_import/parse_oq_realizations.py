3db1326e3258bb451c8572e59604820f34433d4b
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-08-15 17:52:16 +1200

    change data type for realizations, linting, cleanup

--- toshi_hazard_store/oq_import/parse_oq_realizations.py
+++ toshi_hazard_store/oq_import/parse_oq_realizations.py
@@ -6,15 +6,16 @@ NB maybe this belongs in the nzshm_model.psha_adapter.openquake package ??
 
 import collections
 import logging
-from typing import TYPE_CHECKING, Any, Dict
+from typing import TYPE_CHECKING
 
 from nzshm_model import branch_registry
 from nzshm_model.psha_adapter.openquake import gmcm_branch_from_element_text
 
-from .transform import parse_logic_tree_branches
+from .transform import Realization, parse_logic_tree_branches
 
 if TYPE_CHECKING:
     from openquake.calculators.extract import Extractor
+
 
 log = logging.getLogger(__name__)
 
@@ -23,7 +24,7 @@ registry = branch_registry.Registry()
 RealizationRecord = collections.namedtuple('RealizationRecord', 'idx, path, sources, gmms')
 
 
-def build_rlz_mapper(extractor: 'Extractor') -> Dict[int, RealizationRecord]:
+def build_rlz_mapper(extractor: 'Extractor') -> dict[int, RealizationRecord]:
     """
     Builds a realization mapper from an extractor.
 
@@ -31,23 +32,21 @@ def build_rlz_mapper(extractor: 'Extractor') -> Dict[int, RealizationRecord]:
         extractor (Extractor): An OpenQuake Extractor object.
 
     Returns:
-        Dict[int, RealizationRecord]: A dictionary of realization records.
+        dict[int, RealizationRecord]: A dictionary of realization records.
     """
     return get_rlz_mapper(*parse_logic_tree_branches(extractor))
 
 
 def get_rlz_mapper(
-    source_branches: dict[str, str], gsim_branches: dict[str, str], realizations: list[dict[str, Any]]
-) -> Dict[int, RealizationRecord]:
+    source_branches: dict[str, str], gsim_branches: dict[str, str], realizations: list[Realization]
+) -> dict[int, RealizationRecord]:
     gmm_map = build_rlz_gmm_map(gsim_branches)
     source_map = build_rlz_source_map(source_branches)
     rlz_map = build_rlz_map(realizations, source_map, gmm_map)
     return rlz_map
 
 
-# NOTE: this function can mostly stay the same, I think. It simply maps the gsim path (e.g. `gA1`) to an "identity" that we can use
-# input could be a dict?
-def build_rlz_gmm_map(gsim_branches: dict[str, str]) -> Dict[str, branch_registry.BranchRegistryEntry]:
+def build_rlz_gmm_map(gsim_branches: dict[str, str]) -> dict[str, branch_registry.BranchRegistryEntry]:
     rlz_gmm_map = {}
     for gsim_id, gsim in gsim_branches.items():
         log.debug(f"build_rlz_gmm_map(gsim_lt): {gsim_id} {gsim}")
@@ -57,9 +56,7 @@ def build_rlz_gmm_map(gsim_branches: dict[str, str]) -> Dict[str, branch_registr
     return rlz_gmm_map
 
 
-# NOTE: input could be a dict?
-# builds a dict of branch path (e.g. 'AA') to a BranchRegistryEntry. Need the branch string to look up the entry
-def build_rlz_source_map(source_branches: dict[str, str]) -> Dict[str, branch_registry.BranchRegistryEntry]:
+def build_rlz_source_map(source_branches: dict[str, str]) -> dict[str, branch_registry.BranchRegistryEntry]:
     rlz_source_map = dict()
     for source_str in source_branches.values():
         log.debug(f"build_rlz_source_map(source_lt): {source_str}")
@@ -83,7 +80,7 @@ def build_rlz_source_map(source_branches: dict[str, str]) -> Dict[str, branch_re
 
 
 def build_rlz_map(
-    realizations: list[dict[str, Any]],
+    realizations: list[Realization],
     source_map: dict[str, branch_registry.BranchRegistryEntry],
     gmm_map: dict[str, branch_registry.BranchRegistryEntry],
 ) -> dict[int, RealizationRecord]:
@@ -93,11 +90,11 @@ def build_rlz_map(
 
     Args:
         rlz_lt (pandas.DataFrame): The dataframe containing the logic tree branches.
-        source_map (Dict): A map of source identifiers to BranchRegistryEntry objects.
-        gmm_map (Dict): A map of GMM identifiers to BranchRegistryEntry objects.
+        source_map (dict): A map of source identifiers to BranchRegistryEntry objects.
+        gmm_map (dict): A map of GMM identifiers to BranchRegistryEntry objects.
 
     Returns:
-        Dict[int, RealizationRecord]: A dictionary mapping realization indices
+        dict[int, RealizationRecord]: A dictionary mapping realization indices
             to their corresponding RealizationRecord objects.
     """
     # TODO: these realizations only handle one source and one gmm branch. We may want to handle at least multiple
@@ -105,9 +102,9 @@ def build_rlz_map(
     # as when we use a logic tree with an actual branching structure (e.g. extended model)
     rlz_map = dict()
     for rlz in realizations:
-        idx = rlz['ordinal']
+        idx = rlz.ordinal
         path = '_'
-        sources = source_map[rlz['source_path'][0]]
-        gmms = gmm_map[rlz['gsim_path'][0]]
+        sources = source_map[rlz.source_path[0]]
+        gmms = gmm_map[rlz.gsim_path[0]]
         rlz_map[idx] = RealizationRecord(idx=idx, path=path, sources=sources, gmms=gmms)
     return rlz_map
--- toshi_hazard_store/oq_import/transform.py
+++ toshi_hazard_store/oq_import/transform.py
@@ -3,17 +3,25 @@
 Courtesy of Anne Hulsey
 """
 
-import re
 from collections import namedtuple
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
-import numpy as np
-import pandas as pd
+if TYPE_CHECKING:
+    from openquake.calculators.extract import Extractor
 
 CustomLocation = namedtuple("CustomLocation", "site_code lon lat")
 CustomHazardCurve = namedtuple("CustomHazardCurve", "loc poes")
 
 
-def parse_logic_tree_branches(extractor):
+@dataclass
+class Realization:
+    source_path: tuple[str]
+    gsim_path: tuple[str]
+    ordinal: int
+
+
+def parse_logic_tree_branches(extractor: 'Extractor') -> tuple[dict[str, str], dict[str, str], list[Realization]]:
 
     full_lt = extractor.get('full_lt')
     source_model_lt = full_lt.source_model_lt
@@ -22,12 +30,10 @@ def parse_logic_tree_branches(extractor):
     # we don't use the ID, but keeping it as a dict key for symmetry with gsims
     source_branches = {v.id: k for k, v in source_model_lt.branches.items()}
 
-    # NOTE: could be named tuple or dataclass
     gsim_branches = {b.id: str(b.gsim) for b in gslt.branches}
 
-    # NOTE: could be a named tuple or a dataclass
     realizations = [
-        {'source_path': rlz.sm_lt_path, 'gsim_path': rlz.gsim_lt_path, 'ordinal': rlz.ordinal}
+        Realization(source_path=rlz.sm_lt_path, gsim_path=rlz.gsim_lt_path, ordinal=rlz.ordinal)
         for rlz in full_lt.get_realizations()
     ]
 
