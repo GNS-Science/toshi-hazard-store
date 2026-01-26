5eeec88581c97651f30d984ad78fb465a29595eb
Chris B Chamberlain <chrisbc@artisan.co.nz>
2026-01-27 09:03:24 +1300

    add new pydantic model

--- toshi_hazard_store/model/hazard_models_pydantic.py
+++ toshi_hazard_store/model/hazard_models_pydantic.py
@@ -3,12 +3,13 @@
 from datetime import datetime, timezone
 from typing import List
 
-from pydantic import BaseModel, Field, field_validator
-
+from pydantic import BaseModel, Field, field_validator, model_validator
+from nzshm_common.grids import RegionGrid
 from toshi_hazard_store.oq_import.aws_ecr_docker_image import AwsEcrImage
 
 # Float32 = Annotated[float, {"pyarrow_type": pyarrow.float32()}]
 # Int32 = Annotated[int, {"pyarrow_type": pyarrow.int32()}]
+from .constraints import AggregationEnum, IntensityMeasureTypeEnum, VS30Enum
 
 
 class CompatibleHazardCalculation(BaseModel):
@@ -90,3 +91,68 @@ class HazardAggregateCurve(BaseModel):
         if not len(value) == 44:
             raise ValueError(f'expected 44 values but there are {len(value)}')
         return value
+
+
+class GriddedHazardPoeLevels(BaseModel):
+    """A list of hazard acceleration (shaking) at various locations in a grid.
+
+    Ground shaking levels for the given location_grid at the given poe, investigation time, vs30, imt and aggr.
+
+    Attributes:
+        compatible_calc_id: for hazard-calc equivalence.
+        hazard_model_id: the model that the values are derived from.
+        location_grid_id: the NSHM grid identifier.
+        imt: the imt label e.g. `PGA`, `SA(5.0)`.
+        vs30: the VS30 value.
+        aggr: the aggregation type. e.g `mean`, `0.9`, `0.95`.
+        investigation_time: the time period (in years) for which the poe applies. Typically 50 years, but other values are possible.
+        poe: the Probability of Exceedance (poe) expressed as a coeeffient/percentage/ratio (??).
+        accel_levels: a list of floats representing the acceleration level in G at the given poe for each grid location. This list must align with the locations in the given `location_grid_id`.
+    """
+
+    compatible_calc_id: str
+    hazard_model_id: str
+    location_grid_id: str
+
+    imt: str
+    vs30: int
+    aggr: str
+    investigation_time: int
+    poe: float
+
+    accel_levels: List[float]  # was grid_poes, but this was incrreect
+
+    @field_validator('vs30', mode='before')
+    # @classmethod
+    def validate_vs30_value(cls, value: int) -> int:
+        if not value in VS30Enum:
+            raise ValueError(f'vs30 value {value} is not supported')
+        return value
+
+    @field_validator('imt', mode='before')
+    # @classmethod
+    def validate_imt_value(cls, value: str) -> str:
+        if not value in IntensityMeasureTypeEnum:
+            raise ValueError(f'imt value {value} is not supported')
+        return value
+
+    @field_validator('aggr', mode='before')
+    # @classmethod
+    def validate_aggr_value(cls, value: str) -> str:
+        if not value in AggregationEnum:
+            raise ValueError(f'aggr value {value} is not supported')
+        return value
+
+    @field_validator('investigation_time', mode='before')
+    # @classmethod
+    def validate_investigation_time_value(cls, value: int) -> int:
+        if not value == 50:
+            raise ValueError(f'investigation time must be 50 years. {value} is not supported')
+        return value
+
+    @model_validator(mode='before')
+    def validate_len_accel_levels(cls, data) -> List:
+        grid = RegionGrid[data.location_grid_id]
+        if not len(data.accel_levels) == len(grid):
+            raise ValueError(f'expected accel_levels to have `{len(grid)}` values but found: {len(data.accel_levels)}')
+        return data
