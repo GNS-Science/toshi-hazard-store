178ef32a148bea3f302ce5a9892acea6e1a008af
Chris B Chamberlain <chrisbc@artisan.co.nz>
2026-02-24 16:03:16 +1300

    enable skipped grid tests: fix RegionGrid mocking and dataset_cache import

--- tests/model/test_hazard_grid_models.py
+++ tests/model/test_hazard_grid_models.py
@@ -12,29 +12,45 @@ from pyarrow import fs
 
 from toshi_hazard_store.gridded_hazard import gridded_hazard
 from toshi_hazard_store.model.gridded import gridded_hazard_pydantic
-from toshi_hazard_store.model.gridded.gridded_hazard_pydantic import GriddedHazardPoeLevels
+from toshi_hazard_store.model.gridded.gridded_hazard_pydantic import (
+    GriddedHazardPoeLevels,
+)
 from toshi_hazard_store.model.pyarrow import pyarrow_dataset
-from toshi_hazard_store.query import datasets
+from toshi_hazard_store.query import dataset_cache
 
 
-@pytest.mark.skip('WIP on hold, RegionGrid mocking needs work')
-@pytest.mark.parametrize('use64bit', [True, False])
-@pytest.mark.parametrize('validate_model', [True, False])
+@pytest.mark.parametrize("use64bit", [True, False])
+@pytest.mark.parametrize("validate_model", [True, False])
 def test_build_and_roundtrip_gridded_dataset(
-    get_one_degree_region_grid_fixture, tmp_path, monkeypatch, mocker, use64bit, validate_model
+    get_one_degree_region_grid_fixture,
+    tmp_path,
+    monkeypatch,
+    mocker,
+    use64bit,
+    validate_model,
 ):
-    aggr_folder = Path(Path(os.path.realpath(__file__)).parent.parent, 'fixtures', 'aggregate_hazard')
+    aggr_folder = Path(
+        Path(os.path.realpath(__file__)).parent.parent, "fixtures", "aggregate_hazard"
+    )
 
-    monkeypatch.setattr(datasets, 'DATASET_AGGR_URI', str(aggr_folder.absolute()))
-    monkeypatch.setattr(gridded_hazard_pydantic, "DISABLE_GRIDDED_MODEL_VALIDATOR", not validate_model)
+    monkeypatch.setattr(dataset_cache, "DATASET_AGGR_URI", str(aggr_folder.absolute()))
+    monkeypatch.setattr(
+        gridded_hazard_pydantic, "DISABLE_GRIDDED_MODEL_VALIDATOR", not validate_model
+    )
     monkeypatch.setattr(gridded_hazard_pydantic, "USE_64BIT_VALUES", use64bit)
 
-    # Setup RegionGrid Mock
-    mocked_grid_class = mocker.patch("toshi_hazard_store.gridded_hazard.gridded_hazard.RegionGrid")
-    mocked_grid_instance = mocked_grid_class.return_value
-    mocked_grid_instance.load.return_value = get_one_degree_region_grid_fixture
+    # Setup RegionGrid Mock - RegionGrid is an Enum accessed via __getitem__
+    # Convert CodedLocation objects to (lat, lon) tuples as expected by the code
+    grid_tuples = [(loc.lat, loc.lon) for loc in get_one_degree_region_grid_fixture]
 
-    print(mocked_grid_instance.grid())
+    mocked_grid = mocker.MagicMock()
+    mocked_grid.load.return_value = grid_tuples
+    mocked_grid.resolution = 0.1
+
+    mocked_region_grid = mocker.patch(
+        "toshi_hazard_store.gridded_hazard.gridded_hazard.RegionGrid"
+    )
+    mocked_region_grid.__getitem__.return_value = mocked_grid
 
     def get_models():
         # Test helper
@@ -44,12 +60,12 @@ def test_build_and_roundtrip_gridded_dataset(
             #     loc.code for loc in get_one_degree_region_grid_fixture
             # ],  # TODO this field should not be used since only valid locaion_grid should be stored to grid tables
             poe_levels=[0.02],
-            location_grid_id='NZ_0_1_NB_1_1',
-            compatible_calc_id='NZSHM22',
-            hazard_model_id='NSHM_v1.0.4',
+            location_grid_id="NZ_0_1_NB_1_1",
+            compatible_calc_id="NZSHM22",
+            hazard_model_id="NSHM_v1.0.4",
             vs30=400,
-            imt='PGA',
-            agg='mean',
+            imt="PGA",
+            agg="mean",
         ):
             gridded_models.append(record)
         return gridded_models
@@ -69,12 +85,14 @@ def test_build_and_roundtrip_gridded_dataset(
             table,
             output_folder,
             filesystem=filesystem,
-            partitioning=['hazard_model_id'],
+            partitioning=["hazard_model_id"],
             schema=GriddedHazardPoeLevels.pyarrow_schema(),
         )
 
         # read it back in
-        dataset = ds.dataset(output_folder, filesystem=filesystem, format='parquet', partitioning='hive')
+        dataset = ds.dataset(
+            output_folder, filesystem=filesystem, format="parquet", partitioning="hive"
+        )
         table = dataset.to_table()
         df = table.to_pandas()
 
